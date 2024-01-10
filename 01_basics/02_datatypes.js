69cb7a0e04e3889949c4d0a045f59dc9cecd45d4
albertraj13 <127515332+albertraj13@users.noreply.github.com>
2024-01-10 12:59:35 +0000

    complete variables

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // to treat all JS code as newer version
+let name = "Albert"
+let age =  22
+let isLoggedIn = false
+//premitive dataype
+// number 2 to power 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null --> standalone value 
+// undefined value which are not defined
+//symbol -> unique
+
+// non-premitive datatype
+// object
+console.log(typeof null)  // object
+console.log(typeof undefined) //undefined
