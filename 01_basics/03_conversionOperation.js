78371ca6b16d4178fc7a5bf9dbcc06d7b157ba45
albertraj13 <127515332+albertraj13@users.noreply.github.com>
2024-01-10 13:31:09 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score  = "33abc"
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 & false -> 2
+
+let isLoggedIn = "Albert"
+let  booleanValue =  Boolean(isLoggedIn)
+console.log(booleanValue)
+// "" -> false
+//"albert" -> true
+
+
+
