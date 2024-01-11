fe55205ce9aa8c9d8773f18adbc5bfe17fb715c8
albertraj13 <127515332+albertraj13@users.noreply.github.com>
2024-01-11 12:50:41 +0000

    Done Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2 == 2)
+// console.log(2>2)
+// console.log(2<2)
+// console.log(2>=4)
+// console.log(2<=4)
+
+// console.log("2">1)
+// console.log(35<= "35")
+// console.log("35"===35)
+
+// console.log(null>0)
+// console.log(0==null)
+// console.log(null>=0)
+// console.log(45=="45")
+//never perform this conversions
+console.log(null === null)
+console.log(null == undefined)
+
