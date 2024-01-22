4af91354ffabdc1b1b4c00601ec9868bd11b9a87
albertraj13 <127515332+albertraj13@users.noreply.github.com>
2024-01-22 12:17:45 +0000

    Number & Math

---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,15 @@
+const number = 200;
+const balance = new Number(20)
+// console.log(balance);
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+
+// Math +++++++++++++
+
+const number1 = 400
+console.log(Math.ceil(30.33))
+console.log(Math.abs(-23.35))
+console.log(Math.floor(29.90))
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*((max-min+1))+min))
